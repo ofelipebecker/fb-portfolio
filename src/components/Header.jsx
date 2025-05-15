c34c156093b92f755498f81c2a3275c4db24359c
Felipe Becker <ofelipebecker@proton.me>
2025-05-15 16:23:37 -0300

    feat: Add Header component
    Project: Create my React Portfolio

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import logo from "../logo.svg";
+
+const Header = () => {
+    return (
+        <header className="bg-purple">
+            <div className="row">
+                <div className="col-md-3 offset-md-3">English</div>
+                <div className="col-md-3">Português</div>
+            </div>
+            <img src={logo} alt="logo" />
+            <div className="row">
+                <div className="col-md-3 offset-md-3">About</div>
+                <div className="col-md-3">Work</div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
