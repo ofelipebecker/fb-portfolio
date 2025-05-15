08f9cb0d5457a021e85245ecea57c331cd62ddc0
Felipe Becker <ofelipebecker@proton.me>
2025-05-15 16:27:30 -0300

    refactor: Adjust App.js basic structure
    Project: Create my React Portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import Project from "./pages/Project";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/work" element={<Work />} />
+                <Route path="/work/:projectId" element={<Project />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
