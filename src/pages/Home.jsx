f1d904e0e736bda6ee1aa9aa95d57bf486ee7725
Felipe Becker <ofelipebecker@proton.me>
2025-05-19 14:14:03 -0300

    refactor: Add bootstrap logo under table in the Home page
    Project: Create my React Portfolio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,8 +28,12 @@ const Home = () => {
                 <div className="col-md-5">
                     <div className="card border-0 h-100 rounded-3 shadow-sm">
                         <h2 className="mt-3 mb-0">Educação</h2>
-                        <div className="card-body">
+                        <div className="card-body d-flex flex-column">
                             <CourseTable />
+                            <div className="d-flex align-items-center mt-3 text-bs-purple mt-auto">
+                                <span className="bi bi-bootstrap-fill fs-2 me-2"></span>
+                                <span>Bootstrap 5</span>
+                            </div>
                         </div>
                     </div>
                 </div>
