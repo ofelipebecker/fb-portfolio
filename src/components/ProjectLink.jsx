4a3ac6182686ca98dc6692aec981e6df3eb4b148
Felipe Becker <ofelipebecker@proton.me>
2025-05-19 15:40:05 -0300

    refactor: Adjust ProjectLink component structure
    Project: Create my React Portfolio

--- src/components/ProjectLink.jsx
+++ src/components/ProjectLink.jsx
@@ -1,18 +1,42 @@
 import { Link } from "react-router-dom";
 
-function ProjectLink({ title, imageSrc, tags, url }) {
+function ProjectLink({ title, imageSrc, url, tags, disabled }) {
     return (
-        <Link to={url} className="proj-link text-decoration-none text-reset">
-            <div className="card border-0 overflow-hidden rounded-3 shadow-sm mb-5">
-                <div className="card-title d-flex align-items-end mb-0 p-3">
-                    <h2 className="mb-0 me-3">{title}</h2>
-                    <small>{tags}</small>
+        <div className="card border-0 rounded-3 shadow-sm mb-5">
+            {disabled ? (
+                <div
+                    className="card border-0 overflow-hidden rounded-3 shadow-sm position-relative"
+                    style={{ pointerEvents: "none" }}
+                >
+                    <div className="card-title d-flex align-items-end mb-0 p-3 opacity-50">
+                        <h2 className="mb-0 me-3">{title}</h2>
+                        <small>{tags}</small>
+                    </div>
+                    <div className="card-body border-top border-3 p-0 max-h-200px overflow-hidden opacity-25">
+                        <img src={imageSrc} className="w-100" alt={title} />
+                    </div>
+                    <div className="fst-italic position-absolute start-50 top-50 translate-middle text-purple">
+                        <span className="bi bi-cone-striped fs-1 me-2"></span>
+                        <span className="fs-1">Em construção...</span>
+                    </div>
                 </div>
-                <div className="card-body border-top border-3 p-0 max-h-200px overflow-hidden">
-                    <img src={imageSrc} className="w-100" alt={title} />
-                </div>
-            </div>
-        </Link>
+            ) : (
+                <Link
+                    to={url}
+                    className="proj-link text-decoration-none text-reset"
+                >
+                    <div className="card border-0 overflow-hidden rounded-3 shadow-sm">
+                        <div className="card-title d-flex align-items-end mb-0 p-3">
+                            <h2 className="mb-0 me-3">{title}</h2>
+                            <small>{tags}</small>
+                        </div>
+                        <div className="card-body border-top border-3 p-0 max-h-200px overflow-hidden">
+                            <img src={imageSrc} className="w-100" alt={title} />
+                        </div>
+                    </div>
+                </Link>
+            )}
+        </div>
     );
 }
 
