c3f51e3fa312a49f8ca92fa7a9db016a4e0bd7cf
Felipe Becker <ofelipebecker@proton.me>
2025-05-20 15:39:31 -0300

    refactor: Adjust disabled card structure in the ProjectLink component
    Project: Create my React Portfolio

--- src/components/ProjectLink.jsx
+++ src/components/ProjectLink.jsx
@@ -12,12 +12,19 @@ function ProjectLink({ title, imageSrc, url, tags, disabled }) {
                         <h2 className="mb-0 me-3">{title}</h2>
                         <small>{tags}</small>
                     </div>
-                    <div className="card-body border-top border-3 p-0 max-h-200px overflow-hidden opacity-25">
-                        <img src={imageSrc} className="w-100" alt={title} />
-                    </div>
-                    <div className="fst-italic position-absolute start-50 top-50 translate-middle text-purple">
-                        <span className="bi bi-cone-striped fs-1 me-2"></span>
-                        <span className="fs-1">Em construção...</span>
+                    <div className="card-body border-top border-3 p-0 max-h-200px overflow-hidden position-relative bg-light-orange">
+                        <img
+                            src={imageSrc}
+                            className="w-100 opacity-50"
+                            alt={title}
+                        />
+                        <div
+                            class="alert alert-warning fst-italic position-absolute start-50 top-50 translate-middle text-dark-orange p-4"
+                            role="alert"
+                        >
+                            <span className="bi bi-cone-striped me-2"></span>
+                            <span>Em construção...</span>
+                        </div>
                     </div>
                 </div>
             ) : (
