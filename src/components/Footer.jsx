1fc646e01727899bc4f141681e5b991ffd3d59e4
Felipe Becker <ofelipebecker@proton.me>
2025-05-22 15:16:28 -0300

    clean: Create Footer component
    Project: Create my React Portfolio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import ReactLogo from "../assets/images/react-logo.svg";
+
+const Footer = () => {
+    return (
+        <footer class="footer mt-auto py-3 bg-light">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-10 offset-md-1">
+                        <div className="d-flex align-items-center text-purple">
+                            <span>
+                                Felipe Becker | Desenvolvedor Front-end | 2025
+                            </span>
+                            <span className="mx-3">-</span>
+                            <span className="me-2">Feito com</span>
+                            <img
+                                className="w-25px"
+                                src={ReactLogo}
+                                alt="..."
+                            ></img>
+                            <span className="text-react-blue fs-3 fw-semibold ms-2">
+                                React
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import EducationCard from "../components/EducationCard";
+import Footer from "../components/Footer";
 import LocationCard from "../components/LocationCard";
 import SkillsCard from "../components/SkillsCard";
-
-import ReactLogo from "../assets/images/react-logo.svg";
 
 const Home = () => {
     return (
@@ -19,30 +18,7 @@ const Home = () => {
                 <div className="row my-5"></div>
                 <div className="row my-5"></div>
             </div>
-            <footer class="footer mt-auto py-3 bg-light">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-10 offset-md-1">
-                            <div className="d-flex align-items-center text-purple">
-                                <span>
-                                    Felipe Becker | Desenvolvedor Front-end |
-                                    2025
-                                </span>
-                                <span className="mx-3">-</span>
-                                <span className="me-2">Feito com</span>
-                                <img
-                                    className="w-25px"
-                                    src={ReactLogo}
-                                    alt="..."
-                                ></img>
-                                <span className="text-react-blue fs-3 fw-semibold ms-2">
-                                    React
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            <Footer />
         </>
     );
 };
