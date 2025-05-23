3d9916ce9af6058f7b849e1e76f8d545e66de2bc
Felipe Becker <ofelipebecker@proton.me>
2025-05-23 14:28:46 -0300

    refactor: Adjust Home page content responsivity
    Project: Create my React Portfolio pt.2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,15 +7,15 @@ const Home = () => {
         <>
             <div className="container mt-5 text-center">
                 <h1 className="mb-5">Sobre</h1>
-                <div className="row g-5">
+                <div className="row g-5 px-3 px-sm-0">
                     <SkillsCard />
                     <EducationCard />
                 </div>
-                <div className="row">
+                <div className="row px-3 px-sm-0">
                     <LocationCard />
                 </div>
                 <div className="row my-5"></div>
-                <div className="row my-5"></div>
+                <div className="row my-5 d-none d-lg-flex"></div>
             </div>
         </>
     );
