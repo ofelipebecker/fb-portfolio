6af2f4425fd83062bb6f029f5a779e267d8daff0
Felipe Becker <ofelipebecker@proton.me>
2025-05-26 11:41:17 -0300

    refactor: Create separate blocks for image imports in projectData
    Project: Create my React Portfolio

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -1,3 +1,4 @@
+// VC-X Dashboard
 import mainImage from "../assets/images/proj-vcx-dashboard/proj-vcx-dash-1.png";
 import image1STCardTabs from "../assets/images/proj-vcx-dashboard/proj-vcx-dash-5-1-bs-tabs.png";
 import image1STCardChart from "../assets/images/proj-vcx-dashboard/proj-vcx-dash-5-2-chart.png";
@@ -7,6 +8,29 @@ import imageOtherCardsTables from "../assets/images/proj-vcx-dashboard/proj-vcx-
 import imagePeriodSelector from "../assets/images/proj-vcx-dashboard/proj-vcx-dash-6-period-selector.png";
 import imageSketch from "../assets/images/proj-vcx-dashboard/proj-vcx-dash-4-sketch-1.jpg";
 import imageS2 from "../assets/images/proj-vcx-dashboard/proj-vcx-dash-2-0.png";
+
+// Cities & Bikes
+import imageCTB01 from "../assets/images/proj-ctb/proj-ctb-01.png";
+import imageCTB02 from "../assets/images/proj-ctb/proj-ctb-02.png";
+import imageCTB03 from "../assets/images/proj-ctb/proj-ctb-03.png";
+import imageCTB03B from "../assets/images/proj-ctb/proj-ctb-03b.png";
+import imageCTB0401 from "../assets/images/proj-ctb/proj-ctb-04-01.png";
+import imageCTB06 from "../assets/images/proj-ctb/proj-ctb-06.png";
+import imageCTB0701 from "../assets/images/proj-ctb/proj-ctb-07-01.png";
+import imageCTB08 from "../assets/images/proj-ctb/proj-ctb-08.png";
+import imageCTB09 from "../assets/images/proj-ctb/proj-ctb-09.png";
+import imageCTB10 from "../assets/images/proj-ctb/proj-ctb-10.png";
+import imageCTB11 from "../assets/images/proj-ctb/proj-ctb-11.png";
+import imageCTB12 from "../assets/images/proj-ctb/proj-ctb-12.png";
+import imageCTB13 from "../assets/images/proj-ctb/proj-ctb-13.png";
+import imageCTB14 from "../assets/images/proj-ctb/proj-ctb-14.png";
+import imageCTB15 from "../assets/images/proj-ctb/proj-ctb-15.png";
+import imageCTB16 from "../assets/images/proj-ctb/proj-ctb-16.png";
+import imageCTB17 from "../assets/images/proj-ctb/proj-ctb-17.png";
+import imageCTB18 from "../assets/images/proj-ctb/proj-ctb-18.png";
+import imageCTB19 from "../assets/images/proj-ctb/proj-ctb-19.png";
+import imageCTB20 from "../assets/images/proj-ctb/proj-ctb-20.png";
+import imageCTB2001SM from "../assets/images/proj-ctb/proj-ctb-20-01-sm.png";
 
 const projects = {
     project1: {
