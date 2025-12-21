309ca7917f4ee1b5b167815aa8e2001d323199b3
Felipe Becker <ofelipebecker@proton.me>
2025-12-21 15:32:07 -0300

    refactor: Import front-end implementation images in projectsData
    Project: FBP-0825

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -43,6 +43,12 @@ import imageVCXT032 from "../assets/images/proj-vcx-tables/proj-vcx-tables-3-2.p
 import imageVCXT033 from "../assets/images/proj-vcx-tables/proj-vcx-tables-3-3.png";
 import imageVCXT04 from "../assets/images/proj-vcx-tables/proj-vcx-tables-4.png";
 import imageVCXT05 from "../assets/images/proj-vcx-tables/proj-vcx-tables-5.png";
+import imageVCXT071 from "../assets/images/proj-vcx-tables/proj-vcx-tables-7-1.png";
+import imageVCXT072 from "../assets/images/proj-vcx-tables/proj-vcx-tables-7-2.png";
+import imageVCXT073 from "../assets/images/proj-vcx-tables/proj-vcx-tables-7-3.png";
+import imageVCXT074 from "../assets/images/proj-vcx-tables/proj-vcx-tables-7-4.png";
+import imageVCXT075 from "../assets/images/proj-vcx-tables/proj-vcx-tables-7-5.png";
+import imageVCXT076 from "../assets/images/proj-vcx-tables/proj-vcx-tables-7-6.png";
 
 const imagesVCXT03 = [
   {
