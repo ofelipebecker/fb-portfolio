e2f250e0f34d5f602dafac52a560e4872039ea98
Felipe Becker <ofelipebecker@proton.me>
2025-12-23 11:42:14 -0300

    feat: Configure DataTables with pt-br localization and scroll options
    Project: FBP-0825

--- src/components/Project3TableContent.jsx
+++ src/components/Project3TableContent.jsx
@@ -6,8 +6,37 @@ import 'datatables.net-bs5/css/dataTables.bootstrap5.css';
 DataTable.use(DT);
 
 const Project3TableContent = () => {
+    const options = {
+        language: {
+            "decimal": "",
+            "emptyTable": "Nenhum registro disponível na tabela",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
+            "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+            "infoFiltered": "(filtrado de _MAX_ registros totais)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "loadingRecords": "Carregando...",
+            "processing": "Processando...",
+            "search": "Pesquisar",
+            "zeroRecords": "Nenhum registro correspondente encontrado",
+            "paginate": {
+                "first": "Primeira",
+                "last": "Última",
+                "next": "Próxima",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "sortAscending": ": ativar para ordenar coluna ascendente",
+                "sortDescending": ": ativar para ordenar coluna descendente"
+            }
+        },
+        scrollX: true,
+        scrollY: true,
+    };
+
     return (
-        <DataTable>
+        <DataTable options={options}>
             <thead>
                 <tr>
                     <th>Ativo</th>
