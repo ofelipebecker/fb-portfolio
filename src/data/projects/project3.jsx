0f9839938291a57d7c16544d20df23020e247aca
Felipe Becker <ofelipebecker@proton.me>
2026-01-02 13:23:14 -0300

    refactor: Rewrite project conclusion with detailed impact and challenges
    Project: FBP-0825

--- src/data/projects/project3.jsx
+++ src/data/projects/project3.jsx
@@ -246,17 +246,29 @@ const project3 = {
     ],
     conclusion: (
         <>
-            <div className="ratio ratio-16x9">
-                <iframe
-                    src="https://www.youtube.com/embed/XVcYAwtsil8"
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
-            </div>
-            <p className="mb-0 mt-4">
-                Placeholder.
+            <p className="my-4">
+                A padronização das foi um projeto de grande impacto no VC-X Sonar, considerando o 
+                papel central que as tabelas tem no software. A implementação do DataTables.js como 
+                base para todas as tabelas permitiu melhorar a consistência visual e de 
+                comportamento, o que torna mais simples a leitura e compreensão das informações e 
+                reduz o tempo e energia gastos pelos usuários. Além disso, com a padronização, foi 
+                possível adicionar funcionalidades que foram bem recebidas, como: colunas fixas, 
+                melhores opções de exportação de dados, linhas filhas (child rows, linhas dentro de 
+                linhas, que disponibilizam informações extras), filtros por período e 
+                upload/download de arquivos.
             </p>
+            <p className="my-4">
+                O maior desafio deste projeto foi unificar mais de 100 tabelas que estavam 
+                anteriormente divididas entre duas bibliotecas diferentes (a Footable e uma versão 
+                antiga da DataTables.js) em um único padrão atualizado. A diversidade de tipos de 
+                informação e propósitos das tabelas exigiu uma arquitetura flexível que pudesse 
+                atender desde listas simples até processos mais complexos, como upload de arquivos 
+                em tabelas. Adicionalmente, tivemos que implementar tabelas para e-mails via MJML, 
+                um processo separado com suas próprias dificuldades técnicas. Todo esse trabalho foi 
+                realizado por uma equipe reduzida de 3 desenvolvedores que conciliavam este projeto 
+                com outras demandas paralelas.
+            </p>
+            <p className="mb-0 mt-4"></p>
         </>
     ),
 };
