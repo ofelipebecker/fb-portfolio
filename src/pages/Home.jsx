8764ee4c693b202b95951dfabcc3ceb5049947bf
Felipe Becker <ofelipebecker@proton.me>
2026-01-11 18:08:21 -0300

    refactor: Simplify JSX structure in Home page
    Issue: FBP-2601B

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,20 +10,18 @@ const Home = () => {
     const data = homeTranslations[language];
 
     return (
-        <>
-            <div className="container mt-5 text-center">
-                <h1 className="mb-5">{data.home.title}</h1>
-                <div className="row g-5 px-3 px-sm-0">
-                    <SkillsCard />
-                    <EducationCard />
-                </div>
-                <div className="row px-3 px-sm-0">
-                    <LocationCard />
-                </div>
-                <div className="row my-5"></div>
-                <div className="row my-5 d-none d-lg-flex"></div>
+        <div className="container mt-5 text-center">
+            <h1 className="mb-5">{data.home.title}</h1>
+            <div className="row g-5 px-3 px-sm-0">
+                <SkillsCard />
+                <EducationCard />
             </div>
-        </>
+            <div className="row px-3 px-sm-0">
+                <LocationCard />
+            </div>
+            <div className="row my-5"></div>
+            <div className="row my-5 d-none d-lg-flex"></div>
+        </div>
     );
 };
 
