56c974aa26eed2b6ac8bf6bded83c46455514d0b
Felipe Becker <ofelipebecker@proton.me>
2026-01-13 12:52:29 -0300

    feat: Add multilingual support to Work page
    Issue: FBP-2601B

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
+import { useLanguage } from "../context/LanguageContext";
+
 import ProjectLink from "../components/ProjectLink";
 
-import workProjects from "../data/workProjectsData";
-
 const Work = () => {
+    const { language } = useLanguage();
+    const isLanguagePT = language === "pt";
+    const [workProjects, setWorkProjects] = useState(null);
+    
+    useEffect(() => {
+        import(`../data/translations/workProjectsData-${language}.js`)
+            .then(module => setWorkProjects(module.default));
+    }, [language]);
+
+    if (!workProjects) return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-lg-10 offset-lg-1 mt-5">
+                    <p>Carregando...</p>
+                </div>
+            </div>
+        </div>
+    );
+
     return (
         <div className="container my-5">
             <div className="row">
                 <div className="col-12 col-lg-10 offset-lg-1 mt-5">
-                    <h1>Projetos</h1>
+                    <h1>{isLanguagePT ? 'Projetos' : 'Work'}</h1>
                     {workProjects.map((project, index) => (
                         <ProjectLink
                             key={index}
