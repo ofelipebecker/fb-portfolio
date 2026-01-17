d216e193947ce5707496846bd91cb749ac0a299d
Felipe Becker <ofelipebecker@proton.me>
2026-01-17 14:13:35 -0300

    feat: Add projectStepsData for project 3
    Issue: FBP-2601B

---
+++ src/features/projects/project3/data/projectStepsData.js
@@ -0,0 +1,34 @@
+export const projectStepsData = [
+    {
+        title: "Pesquisa e Análise",
+        subtitle: "Referências Sobre Design de Tabelas",
+        image: "proj-vcx-tables-2.png",
+        imageCaption: "Princípios de design para tabelas do Material Design.",
+    },
+    {
+        title: "Pesquisa e Análise",
+        subtitle: "Avaliação Heurística das Tabelas",
+    },
+    {
+        title: "Pesquisa e Análise",
+        subtitle: "Mapas de Calor",
+    },
+    {
+        title: "Modelagem",
+        subtitle: "Desenho de Alternativas",
+        image: "proj-vcx-tables-5.png",
+        imageCaption: "Exemplo de desenho de alternativas: tabela de Ativos.",
+    },
+    {
+        title: "Modelagem",
+        subtitle: "Definição de Diretrizes de Design para as Tabelas",
+    },
+    {
+        title: "Desenvolvimento Front-end",
+        subtitle: "Implementação DatatablesJS + Bootstrap",
+    },
+    {
+        title: "Desenvolvimento Front-end",
+        subtitle: "Implementação de Referência com Dados Sintéticos",
+    },
+];
