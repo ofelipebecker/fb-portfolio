1cb1004c09ea078d1dfea1ad6f47f34e7a4f9486
Felipe Becker <ofelipebecker@proton.me>
2026-01-18 11:21:29 -0300

    refactor: Update ProjectIntro to use intro module structure
    Issue: FBP-2601B

--- src/features/projects/components/ProjectIntro.jsx
+++ src/features/projects/components/ProjectIntro.jsx
@@ -1,21 +1,21 @@
-const ProjectIntro = ({ data }) => {
+const ProjectIntro = ({ intro }) => {
     return (
         <div className="row">
             <div className="col-12 mt-5">
-                <h1 className="mb-4">{data.title}</h1>
+                <h1 className="mb-4">{intro.data.title}</h1>
                 <div className="card border-0 rounded-3 shadow-sm">
                     <div className="card-body d-flex flex-column">
                         <figure className="figure">
                             <img
-                                src={data.image}
+                                src={intro.image}
                                 className="figure-img img-fluid border"
-                                alt={`${data.title} Main`}
+                                alt={`${intro.data.title} Main`}
                             />
                             <figcaption className="figure-caption">
-                                {data.caption}
+                                {intro.data.caption}
                             </figcaption>
                         </figure>
-                        <p className="mb-0 mt-4">{data.text}</p>
+                        <p className="mb-0 mt-4">{intro.data.text}</p>
                     </div>
                 </div>
             </div>
