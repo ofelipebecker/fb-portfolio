de2f7af8584d6be9fcd081a0a7c26b413600d1fe
Felipe Becker <ofelipebecker@proton.me>
2026-01-18 11:20:27 -0300

    feat: Create intro module with image integration
    Issue: FBP-2601B

---
+++ src/features/projects/project3/intro.js
@@ -0,0 +1,7 @@
+import { introData } from './data/introData';
+import imageVCXT01 from '../../../assets/images/proj-vcx-tables/proj-vcx-tables-1.png';
+
+export const intro = {
+    data: introData,
+    image: imageVCXT01,
+};
