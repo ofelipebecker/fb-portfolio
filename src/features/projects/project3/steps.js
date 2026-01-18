de4aee9c7b07a7f43fcf69439c7f3c94bba05adf
Felipe Becker <ofelipebecker@proton.me>
2026-01-18 11:03:13 -0300

    chore: Remove commented imports from steps.js
    Issue: FBP-2601B

--- src/features/projects/project3/steps.js
+++ src/features/projects/project3/steps.js
@@ -1,9 +1,6 @@
 import { StepsContent } from './components/StepsContent';
 import { stepsData } from './data/stepsData';
 
-// import { introData } from "../../features/projects/project3/data/introData";
-// import { conclusionData } from "../../features/projects/project3/data/conclusionData";
-
 export const steps = stepsData.map((data, index) => ({
     data: data,
     content: StepsContent[index],
