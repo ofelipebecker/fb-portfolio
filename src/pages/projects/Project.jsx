060729eb331e2a38e07f550dead416728c6e8a74
Felipe Becker <ofelipebecker@proton.me>
2026-01-18 11:22:30 -0300

    refactor: Update Project page to use intro module
    Issue: FBP-2601B

--- src/pages/projects/Project.jsx
+++ src/pages/projects/Project.jsx
@@ -5,7 +5,7 @@ import ProjectIntro from "../../features/projects/components/ProjectIntro";
 import ProjectPagination from "../../features/projects/components/ProjectPagination";
 import ProjectSteps from "../../features/projects/components/ProjectSteps";
 
-import { introData } from "../../features/projects/project3/data/introData";
+import { intro } from "../../features/projects/project3/intro";
 import { steps } from "../../features/projects/project3/steps";
 
 const Project = () => {
@@ -16,11 +16,11 @@ const Project = () => {
         <div className="container">
             <div className="row">
                 <div className="col-12 col-lg-10 offset-lg-1 mt-5 px-3 px-sm-0">
-                    <ProjectBreadcrumb projectTitle={introData.title} />
-                    <ProjectIntro data={introData} /> 
+                    <ProjectBreadcrumb projectTitle={intro.data.title} />
+                    <ProjectIntro intro={intro} /> 
                     <ProjectSteps steps={projectSteps} />
                     <div className="d-flex justify-content-between align-items-center">
-                        <ProjectBreadcrumb projectTitle={introData.title} />
+                        <ProjectBreadcrumb projectTitle={intro.data.title} />
                         <ProjectPagination currentProjectId={projectId} />
                     </div>
                     <div className="row my-5"></div>
