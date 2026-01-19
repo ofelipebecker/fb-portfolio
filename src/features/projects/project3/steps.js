12fbb3a2a25cc778ce9cb732fd19aca75fed8109
Felipe Becker <ofelipebecker@proton.me>
2026-01-19 12:51:18 -0300

    chore: Change steps export from named to default
    Issue: FBP-2601B

--- src/features/projects/project3/steps.js
+++ src/features/projects/project3/steps.js
@@ -2,8 +2,10 @@ import { StepsContent } from './components/StepsContent';
 import { stepsData } from './data/stepsData';
 import { stepsImages } from './stepsImages';
 
-export const steps = stepsData.map((data, index) => ({
+const steps = stepsData.map((data, index) => ({
     data: data,
     content: StepsContent[index],
     images: stepsImages[index],
 }));
+
+export default steps;
