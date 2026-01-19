8e8509623c1b40a015eacd5e19a2413048fd67e2
Felipe Becker <ofelipebecker@proton.me>
2026-01-19 11:08:29 -0300

    feat: Create stepsImages module for centralized image imports
    Issue: FBP-2601B

---
+++ src/features/projects/project3/stepsImages.js
@@ -0,0 +1,21 @@
+import imageVCXT02 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-2.png";
+import imageVCXT031 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-3-1.png";
+import imageVCXT032 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-3-2.png";
+import imageVCXT033 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-3-3.png";
+import imageVCXT04 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-4.png"
+import imageVCXT05 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-5.png";
+import imageVCXT071 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-7-1.png";
+import imageVCXT072 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-7-2.png";
+import imageVCXT073 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-7-3.png";
+import imageVCXT074 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-7-4.png";
+import imageVCXT075 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-7-5.png";
+import imageVCXT076 from "../../../assets/images/proj-vcx-tables/proj-vcx-tables-7-6.png";
+
+export const stepsImages = [
+    [imageVCXT02],
+    [imageVCXT031, imageVCXT032, imageVCXT033],
+    [imageVCXT04],
+    [imageVCXT05],
+    [],
+    [imageVCXT071, imageVCXT072, imageVCXT073, imageVCXT074, imageVCXT075, imageVCXT076],
+];
