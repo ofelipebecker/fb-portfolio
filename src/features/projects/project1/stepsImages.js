e406df0277a780b21df24d8bf4ff26c954107aac
Felipe Becker <ofelipebecker@proton.me>
2026-01-19 17:51:13 -0300

    feat: Create stepsImages module for project1 feature
    Issue: FBP-2601B

---
+++ src/features/projects/project1/stepsImages.js
@@ -0,0 +1,28 @@
+import imageVCXD02 from '../../../assets/images/proj-vcx-dashboard/proj-vcx-dash-2-0.png';
+import imageVCXD03 from '../../../assets/images/proj-vcx-dashboard/proj-vcx-dash-3-heatmap.jpg';
+import imageVCXD04 from '../../../assets/images/proj-vcx-dashboard/proj-vcx-dash-4-sketch-1.jpg';
+import imageVCXD051 from '../../../assets/images/proj-vcx-dashboard/proj-vcx-dash-5-1-bs-tabs.png';
+import imageVCXD052 from '../../../assets/images/proj-vcx-dashboard/proj-vcx-dash-5-2-chart.png';
+import imageVCXD06 from '../../../assets/images/proj-vcx-dashboard/proj-vcx-dash-6-period-selector.png';
+import imageVCXD07 from '../../../assets/images/proj-vcx-dashboard/proj-vcx-dash-7-other-charts-tabs.png';
+import imageVCXD08 from '../../../assets/images/proj-vcx-dashboard/proj-vcx-dash-8-other-charts-tables.png';
+
+export const stepsImages = [
+    [],
+    [imageVCXD02],
+    [],
+    [imageVCXD03],
+    [],
+    [],
+    [imageVCXD04],
+    [],
+    [],
+    [imageVCXD051],
+    [imageVCXD052],
+    [],
+    [imageVCXD06],
+    [],
+    [imageVCXD07],
+    [imageVCXD08],
+    [],
+];
