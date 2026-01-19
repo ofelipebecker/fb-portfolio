026157445309291634f442c7b3723d0acb950293
Felipe Becker <ofelipebecker@proton.me>
2026-01-19 18:07:32 -0300

    feat: Create intro module for project1 feature
    Issue: FBP-2601B

---
+++ src/features/projects/project1/intro.js
@@ -0,0 +1,9 @@
+import { introData } from './data/introData';
+import imageVCXD01 from '../../../assets/images/proj-vcx-dashboard/proj-vcx-dash-1.png';
+
+const intro = {
+    data: introData,
+    image: imageVCXD01,
+};
+
+export default intro;
