78ddfeab83092380abaed40cb2cf4a5406bf99ad
Felipe Becker <ofelipebecker@proton.me>
2026-01-19 11:09:43 -0300

    feat: Integrate stepsImages into steps module
    Issue: FBP-2601B

--- src/features/projects/project3/steps.js
+++ src/features/projects/project3/steps.js
@@ -1,7 +1,9 @@
 import { StepsContent } from './components/StepsContent';
 import { stepsData } from './data/stepsData';
+import { stepsImages } from './stepsImages';
 
 export const steps = stepsData.map((data, index) => ({
     data: data,
     content: StepsContent[index],
+    images: stepsImages[index],
 }));
