8168298dbaa21d89107c03934c596da8d98b8a15
Felipe Becker <ofelipebecker@proton.me>
2026-01-20 09:32:51 -0300

    feat: Create intro module for project2 feature
    Issue: FBP-2601B

---
+++ src/features/projects/project2/intro.js
@@ -0,0 +1,9 @@
+import { introData } from './data/introData';
+import imageCTB01 from '../../../assets/images/proj-ctb/proj-ctb-01.png';
+
+const intro = {
+    data: introData,
+    image: imageCTB01,
+};
+
+export default intro;
