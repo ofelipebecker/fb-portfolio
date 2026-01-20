ef2e007180ea491504ab6a5a58b1f6a308371fdd
Felipe Becker <ofelipebecker@proton.me>
2026-01-20 09:44:49 -0300

    feat: Create stepsImages module for project2 feature
    Issue: FBP-2601B

---
+++ src/features/projects/project2/stepsImages.js
@@ -0,0 +1,45 @@
+import imageCTB02 from "../../../assets/images/proj-ctb/proj-ctb-02.png";
+import imageCTB031 from "../../../assets/images/proj-ctb/proj-ctb-03.png";
+import imageCTB032 from "../../../assets/images/proj-ctb/proj-ctb-03b.png";
+import imageCTB041 from "../../../assets/images/proj-ctb/proj-ctb-04-01.png";
+import imageCTB06 from "../../../assets/images/proj-ctb/proj-ctb-06.png";
+import imageCTB071 from "../../../assets/images/proj-ctb/proj-ctb-07-01.png";
+import imageCTB08 from "../../../assets/images/proj-ctb/proj-ctb-08.png";
+import imageCTB09 from "../../../assets/images/proj-ctb/proj-ctb-09.png";
+import imageCTB10 from "../../../assets/images/proj-ctb/proj-ctb-10.png";
+import imageCTB11 from "../../../assets/images/proj-ctb/proj-ctb-11.png";
+import imageCTB12 from "../../../assets/images/proj-ctb/proj-ctb-12.png";
+import imageCTB13 from "../../../assets/images/proj-ctb/proj-ctb-13.png";
+import imageCTB14 from "../../../assets/images/proj-ctb/proj-ctb-14.png";
+import imageCTB15 from "../../../assets/images/proj-ctb/proj-ctb-15.png";
+import imageCTB16 from "../../../assets/images/proj-ctb/proj-ctb-16.png";
+import imageCTB17 from "../../../assets/images/proj-ctb/proj-ctb-17.png";
+import imageCTB18 from "../../../assets/images/proj-ctb/proj-ctb-18.png";
+import imageCTB19 from "../../../assets/images/proj-ctb/proj-ctb-19.png";
+import imageCTB20 from "../../../assets/images/proj-ctb/proj-ctb-20.png";
+import imageCTB21 from "../../../assets/images/proj-ctb/proj-ctb-20-01-sm.png";
+
+export const stepsImages = [
+    [imageCTB02],
+    [imageCTB031],
+    [],
+    [imageCTB032],
+    [imageCTB041],
+    [],
+    [imageCTB06],
+    [imageCTB071],
+    [imageCTB08],
+    [imageCTB09],
+    [imageCTB10],
+    [imageCTB11],
+    [imageCTB12],
+    [imageCTB13],
+    [imageCTB14],
+    [imageCTB15],
+    [imageCTB16],
+    [imageCTB17],
+    [imageCTB18],
+    [imageCTB19],
+    [imageCTB20],
+    [imageCTB21],
+];
