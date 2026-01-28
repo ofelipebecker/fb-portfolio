e032afd10b90323e70ebded5fb9511a0796af9c7
Felipe Becker <ofelipebecker@proton.me>
2026-01-28 19:14:51 -0300

    feat: Implement factory modules for project3 multilingual support
    Issue: FBP-2601C

--- src/features/projects/project3/steps.js
+++ src/features/projects/project3/steps.js
@@ -1,11 +1,15 @@
-import { StepsContent } from './components/StepsContent';
-import { stepsData } from './data/stepsData';
-import { stepsImages } from './stepsImages';
+const createSteps = (language) => {
+  const StepsContent = require(`./components/${language}/StepsContent.jsx`).StepsContent;
+  const stepsData = require(`./data/${language}/stepsData.js`).stepsData;
+  const stepsImages = require('./stepsImages').stepsImages;
 
-const steps = stepsData.map((data, index) => ({
-  data: data,
-  content: StepsContent[index],
-  images: stepsImages[index],
-}));
+  const steps = stepsData.map((data, index) => ({
+    data: data,
+    content: StepsContent[index],
+    images: stepsImages[index],
+  }));
 
-export default steps;
+  return steps;
+};
+
+export default createSteps;
