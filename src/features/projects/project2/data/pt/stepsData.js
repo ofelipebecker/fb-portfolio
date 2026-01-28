664e0e66937ecb897688f4e94fcd454b7a14c6e5
Felipe Becker <ofelipebecker@proton.me>
2026-01-28 13:23:55 -0300

    feat: Implement multilingual data structure for project2
    Issue: FBP-2601C

---
+++ src/features/projects/project2/data/pt/stepsData.js
@@ -0,0 +1,110 @@
+export const stepsData = [
+  {
+    title: 'Pesquisa e Análise',
+    subtitle: 'Benchmarking',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Pesquisa e Análise',
+    subtitle: 'Análise de Tecnologias de Mapas',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Pesquisa e Análise',
+    subtitle: 'Análise de Tecnologias de Desenvolvimento',
+  },
+  {
+    title: 'Pesquisa e Análise',
+    subtitle: 'Descrição de Tarefas dos Usuários',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Modelagem',
+    subtitle: 'Desenho de Alternativas',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Modelagem',
+    subtitle: 'Prototipagem',
+  },
+  {
+    title: 'Modelagem',
+    subtitle: 'Novo Nome',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Modelagem',
+    subtitle: 'Rebranding',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento do Mapa',
+    subtitle: 'Revisão e Atualização de Dados no OpenStreetMap',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento do Mapa',
+    subtitle: 'Geração dos Arquivos GeoJSON',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento do Mapa',
+    subtitle: 'Criação de Datasets no Mapbox',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento do Mapa',
+    subtitle: 'Criação de Tilesets no Mapbox',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento do Mapa',
+    subtitle: 'Criação do Estilo de Mapa',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento Front-end',
+    subtitle: 'HTML Básico e Inserção do Mapa',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento Front-end',
+    subtitle: 'Barras de Navegação Superior e Lateral (Drawer)',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento Front-end',
+    subtitle: 'Modais do Mapa',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento Front-end',
+    subtitle: 'Ferramenta de Pesquisa',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento Front-end',
+    subtitle: 'Rotas',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento Front-end',
+    subtitle: 'Rotas e Localização do Usuário',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento Front-end',
+    subtitle: 'Botão de Chamada Telefônica',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento Front-end',
+    subtitle: 'Informação de Clima',
+    imagesCaptions: [''],
+  },
+  {
+    title: 'Desenvolvimento Front-end',
+    subtitle: 'Hospedagem e Publicação',
+    imagesCaptions: [''],
+  },
+];
