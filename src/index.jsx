8183dd06b1208f354a20a89908b0d83e6a26e748
Felipe Becker <ofelipebecker@proton.me>
2026-07-21 17:08:31 -0300

    refactor: Remove Bootstrap CSS and JS imports from entry point
    Branch: FBP-2607A
    Issue: #8

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
 import App from './App';
 
